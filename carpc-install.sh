9ab94cc2abfc53db5606c76c04202a40a23498fe
Can Yilmaz <spawnedc@gmail.com>
2016-04-02 10:43:48 +0100

    Check if dirs exit before building

--- carpc-install.sh
+++ carpc-install.sh
@@ -3,14 +3,8 @@
 # free some space
 sudo apt-get --yes --force-yes remove --purge minecraft-pi scratch wolfram-engine debian-reference-* epiphany-browser* sonic-pi supercollider*
 
-# navit dev
-sudo apt-get --yes install git imagemagick libdbus-1-dev libdbus-glib-1-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev libimlib2-dev librsvg2-bin libspeechd-dev libxml2-dev ttf-liberation libgtk2.0-dev gcc g++ cmake make zlib1g-dev libpng12-dev libsdl-image1.2-dev libdevil-dev libglc-dev freeglut3-dev libxmu-dev libgps-dev
-
 # install Kodi dependencies
 sudo apt-get --yes --force-yes install libssh-4 libmicrohttpd10 libtinyxml2.6.2 libyajl2 libmysqlclient18 liblzo2-2 libpcrecpp0 libmysqlclient-dev
-
-# Kodi dev
-sudo apt-get --yes install libboost1.50-all swig curl libgnutls-dev libxslt1-dev libmpeg2-4-dev libmad0-dev libjpeg8-dev libsamplerate0-dev libogg-dev libvorbis-dev libflac-dev libtiff4-dev liblzo2-dev zip unzip libsqlite3-dev libpcre3-dev libjasper-dev libsdl1.2-dev libass-dev libmodplug-dev libcdio-dev libtinyxml2-dev libyajl-dev libgpg-error-dev libgcrypt11-dev libmicrohttpd-dev autoconf libtool autopoint libudev-dev python-dev python-imaging libcurl4-gnutls-dev libbz2-dev libtinyxml-dev libssh-dev libxrandr-dev libsmbclient-dev libcap-dev gawk gperf debhelper libiso9660-dev liblockdev1-dev ccache gcc-4.8 g++-4.8 libparted-dev
 
 # Send click events to X windows
 sudo apt-get --yes --force-yes install xdotool
@@ -21,19 +15,30 @@ sudo apt-get --yes --force-yes install gpsd gpsd-clients
 # espeak
 sudo apt-get --yes --force-yes install espeak
 
+NAVIT_BUILD_DIR="/home/pi/navit-build"
+
 # Download and build Navit
-cd ~
-git clone https://github.com/navit-gps/navit.git
-mkdir navit-build && cd navit-build
-cmake -DFREETYPE_INCLUDE_DIRS=/usr/include/freetype2/ --enable-map-csv ~/navit
-make -j4
+if [ ! -d "$NAVIT_BUILD_DIR" ]; then
+  # navit dev
+  sudo apt-get --yes install git imagemagick libdbus-1-dev libdbus-glib-1-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev libimlib2-dev librsvg2-bin libspeechd-dev libxml2-dev ttf-liberation libgtk2.0-dev gcc g++ cmake make zlib1g-dev libpng12-dev libsdl-image1.2-dev libdevil-dev libglc-dev freeglut3-dev libxmu-dev libgps-dev
+
+  cd ~
+  git clone https://github.com/navit-gps/navit.git
+  mkdir navit-build && cd navit-build
+  cmake -DFREETYPE_INCLUDE_DIRS=/usr/include/freetype2/ --enable-map-csv ~/navit
+  make -j4
+fi
+
+WIRINGPI_BUILD_DIR="/home/pi/wiringPi"
 
 # Install wiringPI
-cd ~
-git clone git://git.drogon.net/wiringPi
-cd wiringPi
-git pull origin
-./build
+if [ ! -d "$WIRINGPI_BUILD_DIR" ]; then
+  cd ~
+  git clone git://git.drogon.net/wiringPi
+  cd wiringPi
+  git pull origin
+  ./build
+fi
 
 # Cleanup
 sudo apt-get --yes autoremove
